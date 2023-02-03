a1d7c8b558cbdf13cae060af59749a5077df5276
Fábio Vicente <fab10_lima@hotmail.com>
2023-02-03 14:50:44 -0300

    refactor(Error): :recycle: provide instance without message

--- src/back-end/error/Error.ts
+++ src/back-end/error/Error.ts
@@ -4,11 +4,11 @@ import ErrorCatalog from './ErrorCatalog';
 export default class Error implements IError {
   code: number;
 
-  message: string;
+  message: string | null;
 
   static catalog = ErrorCatalog;
 
-  constructor(code: number, message: string) {
+  constructor(code: number, message: string | null = null) {
     this.code = code;
     this.message = message;
   }
