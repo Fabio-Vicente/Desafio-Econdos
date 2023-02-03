99c9443c6de1c9bb28384fa8cb313a14c6b657e8
Fábio Vicente <fab10_lima@hotmail.com>
2023-02-02 22:40:10 -0300

    build: :construction_worker: use enviroment files

--- src/back-end/database/MongoConnection.ts
+++ src/back-end/database/MongoConnection.ts
@@ -1,8 +1,13 @@
+import 'dotenv/config';
 import mongoose from 'mongoose';
 
 const mongoDBPort = process.env.MONGOPORT?.toString();
+const mongoDBHost = process.env.MONGOHOST?.toString();
+const mongoDBPassword = process.env.MONGOPASSWORD?.toString();
+const mongoDBUser = process.env.MONGOUSER?.toString();
+
 // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
-const URL: string = `mongodb://localhost:${mongoDBPort}/eCondosFriends`;
+const URL: string = `mongodb://${mongoDBUser}:${mongoDBPassword}@${mongoDBHost}:${mongoDBPort}`;
 
 export default async function connectDatabse(): Promise<void> {
   await mongoose.connect(URL);
