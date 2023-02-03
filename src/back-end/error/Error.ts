5ec0873c60e7c9bb226fc8ffc66f3de7b5304970
Fábio Vicente <fab10_lima@hotmail.com>
2023-02-03 14:47:04 -0300

    refactor(Error): :recycle: include message prop

--- src/back-end/error/Error.ts
+++ src/back-end/error/Error.ts
@@ -4,9 +4,12 @@ import ErrorCatalog from './ErrorCatalog';
 export default class Error implements IError {
   code: number;
 
+  message: string;
+
   static catalog = ErrorCatalog;
 
-  constructor(code: number) {
+  constructor(code: number, message: string) {
     this.code = code;
+    this.message = message;
   }
 }
--- src/back-end/interfaces/IError.ts
+++ src/back-end/interfaces/IError.ts
@@ -1,3 +1,4 @@
 export default interface IError {
   code: number
+  message: string
 }
