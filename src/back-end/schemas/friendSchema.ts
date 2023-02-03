01f741ab9ddf485efb3f6bfc4b060c41a24b469b
Fábio Vicente <fab10_lima@hotmail.com>
2023-02-03 18:31:41 -0300

    feat(Friend): :safety_vest: improve schema
    provide rules for mongo id

--- src/back-end/schemas/friendSchema.ts
+++ src/back-end/schemas/friendSchema.ts
@@ -1,8 +1,11 @@
+import { isValidObjectId } from 'mongoose';
 import z from 'zod';
 
 const friendSchema = z.object({
+  _id: z.string().refine(isValidObjectId, 'Invalid id').optional(),
   name: z.string(),
   email: z.string().email(),
+  secretFriend: z.string().refine(isValidObjectId, 'Invalid id').optional(),
 });
 
 export default friendSchema;
