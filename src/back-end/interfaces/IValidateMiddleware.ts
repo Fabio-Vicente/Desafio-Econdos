6749f92acd1909368e07b3f97d929f5ac959006e
Fábio Vicente <fab10_lima@hotmail.com>
2023-02-03 17:30:13 -0300

    refactor: :recycle: refactor ValidateMiddleware
    provide optional method `validateId`

--- src/back-end/interfaces/IValidateMiddleware.ts
+++ src/back-end/interfaces/IValidateMiddleware.ts
@@ -2,4 +2,5 @@ import { type NextFunction, type Request, type Response } from 'express';
 
 export default interface IValidateMiddleware<T> {
   validateContent: (req: Request<T>, res: Response, next: NextFunction) => void
+  validateId?: (req: Request, res: Response, next: NextFunction) => void
 }
