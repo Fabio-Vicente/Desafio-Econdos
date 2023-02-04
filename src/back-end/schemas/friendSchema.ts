8fd311151b84c35dc94a451ab05493e078451ae6
Fábio Vicente <fab10_lima@hotmail.com>
2023-02-04 01:58:47 -0300

    refactor(Friend): :speech_balloon: refactor schema
    switch literals messages by cataloged ones

--- src/back-end/schemas/friendSchema.ts
+++ src/back-end/schemas/friendSchema.ts
@@ -1,11 +1,12 @@
 import { isValidObjectId } from 'mongoose';
 import z from 'zod';
+import { ErrorCatalog } from '../error';
 
 const friendSchema = z.object({
-  _id: z.string().refine(isValidObjectId, 'Invalid id').optional(),
+  _id: z.string().refine(isValidObjectId, ErrorCatalog.INVALID_ID).optional(),
   name: z.string(),
   email: z.string().email(),
-  secretFriend: z.string().refine(isValidObjectId, 'Invalid id').optional(),
+  secretFriend: z.string().refine(isValidObjectId, ErrorCatalog.INVALID_ID).optional(),
 });
 
 export default friendSchema;
