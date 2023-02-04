3d6a495668027fe349bcc8804b4d63320a1ae9a1
Fábio Vicente <fab10_lima@hotmail.com>
2023-02-04 01:55:01 -0300

    fix(Friend): :card_file_box: fix database model
    provide missing model implementation

--- src/back-end/repository/FriendRepository.ts
+++ src/back-end/repository/FriendRepository.ts
@@ -2,7 +2,22 @@ import mongoose, { Schema, type Model } from 'mongoose';
 import type { IFriend } from '../interfaces/core';
 import MongoDBFactory from './factory/MongoDBFactory';
 
-const schema = new Schema<IFriend>();
+const schema = new Schema<IFriend>({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  secretFriend: {
+    type: mongoose.Types.ObjectId,
+    required: false,
+  },
+}, {
+  versionKey: false,
+});
 
 export default class FriendRepository extends MongoDBFactory<IFriend> {
   constructor(readonly model: Model<IFriend> = mongoose.model('Friends', schema)) {
