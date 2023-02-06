35b60c84459278bd2a3caadd9705c2f073353b6e
Fábio Vicente <fab10_lima@hotmail.com>
2023-02-06 15:01:19 -0300

    test(Home): :recycle: refactor tests

--- tests/front-end/integration/Home.spec.tsx
+++ tests/front-end/integration/Home.spec.tsx
@@ -1,9 +1,11 @@
+import '@testing-library/jest-dom';
+import React from 'react';
 import { render, screen } from '@testing-library/react';
-import { Home } from '../../src/front-end/pages';
+import { Home } from 'desafio-econdos-frontend/src/pages';
 
 describe('Verifica se na página principal', () => {
   it('tem tabela para preenchimentos dos amigos', () => {
-    render(Home);
+    render(<Home />);
 
     expect(screen.getByRole('table')).toBeInTheDocument();
   });
