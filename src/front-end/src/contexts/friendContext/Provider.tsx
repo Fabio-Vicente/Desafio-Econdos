d8d31d9697e3000391682ea30d77f7f612bb0087
Fábio Vicente <fab10_lima@hotmail.com>
2023-02-07 01:06:29 -0300

    style: :truck: rename backend's service names

--- src/front-end/src/contexts/friendContext/Provider.tsx
+++ src/front-end/src/contexts/friendContext/Provider.tsx
@@ -3,21 +3,26 @@ import React, {
 } from 'react';
 import friendContext from '.';
 import type IFriend from '../../contracts/core/IFriend';
+import { createFriendRegister, deleteFriendRegister, updateFriendRegister } from '../../services/BackendRequest';
 
 export default function FriendProvider({ children }: { children: ReactNode }): ReactElement {
   const [friends, setFriends] = useState<IFriend[]>([]);
 
   const contextValue = useMemo(() => {
-    const insertFriend = (friend: IFriend): void => {
-      setFriends([...friends, friend]);
+    const insertFriend = async (friend: IFriend): Promise<void> => {
+      const registeredFriend: IFriend = await createFriendRegister(friend);
+      setFriends([...friends, registeredFriend]);
     };
 
-    const deleteFriend = (index: number): void => {
+    const deleteFriend = async (index: number): Promise<void> => {
+      await deleteFriendRegister(friends[index]);
       setFriends([...friends.slice(0, index), ...friends.slice(index + 1)]);
     };
 
-    const updateFriend = (friend: IFriend, index: number): void => {
-      setFriends([...friends.slice(0, index), friend, ...friends.slice(index + 1)]);
+    const updateFriend = async (friend: IFriend, index: number): Promise<void> => {
+      const updatedFriend = { ...friend, _id: friends[index]._id };
+      await updateFriendRegister(updatedFriend);
+      setFriends([...friends.slice(0, index), updatedFriend, ...friends.slice(index + 1)]);
     };
 
     return {
